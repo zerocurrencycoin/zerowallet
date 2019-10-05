25c5513330bf8d7927de530b75b73e3fbe785a47
miodragpop <miodragpopovic@stcable.net>
2019-10-05 20:22:13 +0200

    no need for restart to apply theme

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -525,8 +525,6 @@ void MainWindow::setupSettingsModal() {
 
         QObject::connect(settings.comboBoxTheme, &QComboBox::currentTextChanged, [=] (QString theme_name) {
             this->slot_change_theme(theme_name);
-            // Tell the user to restart
-            QMessageBox::information(this, tr("Restart"), tr("Please restart ZecWallet to have the theme apply"), QMessageBox::Ok);
         });
 
         // Save sent transactions
