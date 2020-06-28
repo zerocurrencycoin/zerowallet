e9da854e3885a079b5165e1a4aee6d2e03e07edc
Cryptoforge <cryptoforge.cc@gmail.com>
2020-06-27 22:05:06 -0700

    fix dotranslations.sh

--- src/scripts/dotranslations.sh
+++ src/scripts/dotranslations.sh
@@ -11,7 +11,7 @@ $QT_STATIC/bin/lrelease zero-qt-wallet.pro
 # Then update the qt base translations. First, get all languages
 ls res/*.qm | awk -F '[_.]' '{print $4}' | while read -r language ; do
     if [ -f $QT_STATIC/translations/qtbase_$language.qm ]; then
-        $QT_STATIC/bin/lconvert -o res/zero-qt-wallet_$language.qm $QT_STATIC/translations/qtbase_$language.qm res/zero-qt-wallet_$language.qm
+        $QT_STATIC/bin/lconvert -o res/zero_qt_wallet_$language.qm $QT_STATIC/translations/qtbase_$language.qm res/zero_qt_wallet_$language.qm
         #mv res/zero-qt-wallet_$language.qm res/zero-qt-wallet_$language.qm
     fi
 done
