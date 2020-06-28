a175a3ff768ac42ffe168f3916e00ffae777b204
Cryptoforge <cryptoforge.cc@gmail.com>
2020-06-27 22:04:45 -0700

    update libsodium 1.0.18

--- res/libsodium/buildlibsodium-win.sh
+++ res/libsodium/buildlibsodium-win.sh
@@ -9,17 +9,20 @@ echo "Building libsodium"
 
 # Go into the lib sodium directory
 cd res/libsodium
-if [ ! -f libsodium-1.0.16.tar.gz ]; then
-    wget https://download.libsodium.org/libsodium/releases/old/libsodium-1.0.16.tar.gz
+if [ ! -f libsodium-1.0.18.tar.gz ]; then
+    wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
 fi
 
 if [ ! -d win ]; then
     mkdir win
-    tar -C ./win -xf libsodium-1.0.16.tar.gz
+    tar -C ./win -xf libsodium-1.0.18.tar.gz
+else
+    rm -r win/libsodium-1.0.18
+    tar -C ./win -xf libsodium-1.0.18.tar.gz
 fi
 
 # Now build it
-cd ./win/libsodium-1.0.16
+cd ./win/libsodium-1.0.18
 
 export HOST=x86_64-w64-mingw32
 CXX=x86_64-w64-mingw32-g++-posix
@@ -28,11 +31,11 @@ PREFIX="$(pwd)/depends/$HOST"
 
 LIBS="" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 CC="${CC} -g " CXX="${CXX} -g " > /dev/null
 
-make clean > /dev/null
-make > /dev/null
+make clean > /dev/null 2>&1
+make > /dev/null 2>&1
 
 cd ..
 cd ..
 
 # copy the library to the parents's res/ folder
-cp win/libsodium-1.0.16/src/libsodium/.libs/libsodium.a ../
+cp win/libsodium-1.0.18/src/libsodium/.libs/libsodium.a ../
